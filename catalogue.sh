0fb4f9bf2cfb0a1a0c61dd225254e1664157ab50
Sree-ramya <ksreeramya830@gmail.com>
2023-12-18 18:39:10 +0530

    roboshop shell scripts

--- catalogue.sh
+++ catalogue.sh
@@ -92,6 +92,6 @@ dnf install mongodb-org-shell -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB client"
 
-mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
+mongo --host $mongodb.ramya.website</app/schema/catalogue.js &>> $LOGFILE
 
 VALIDATE $? "Loading catalouge data into MongoDB"

33e17275e44763bab43a00d3537d59217b411ae0
Sree-ramya <ksreeramya830@gmail.com>
2023-12-19 17:49:06 +0530

    shell scripting for roboshop project

--- redis.sh
+++ redis.sh
@@ -41,7 +41,7 @@ dnf install redis -y &>> $LOGFILE
 
 VALIDATE $? "Installed redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>> $LOGFILE
 
 VALIDATE $? "Remote access to redis"
 
